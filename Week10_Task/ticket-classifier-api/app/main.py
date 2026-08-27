3476d6246e89ccde5dd4c41c16882a9da2d1316a
Dina <dinakarant09@gmail.com>
2026-08-27 10:31:14 +0530

    Add VSCode settings for Python environment management and update main.py imports for consistency

--- Week10_Task/ticket-classifier-api/app/main.py
+++ Week10_Task/ticket-classifier-api/app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Request, HTTPException
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles 
+from fastapi.templating import Jinja2Templates 
+from fastapi.responses import HTMLResponse 
  
 from app.schemas import TicketRequest, TicketResponse
 from app.predict import predict_category
